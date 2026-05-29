156edfcae687070e4df614cfd0e8dbaddba0ed82
haolifeng <haolifengwang@126.com>
2026-05-29 16:58:36 +0800

    add config-main and config-test
    (cherry picked from commit 223ee1fe4dfbdcd860946a097fd9e400d5694b13)

---
+++ evm-token-transfer/evm-cli-v4/config-test.js
@@ -0,0 +1,18 @@
+let config = {
+    tokenTransferScAddr:'0x174BADB1B8b9248dAe0519C5C8f9fFd9aCb2E779',
+
+    gateWayScAddr:"0xDDddd58428706FEdD013b3A761c6E40723a7911d",
+    nodeUrl:"https://gwan-ssl.wandevs.org:46891",
+
+
+    solChainID : 2147484149,
+    SolScAddr : "4qyZxqVyE4JsjoW3jgQFqcmuygUUM1hMNUASobcabgC8",
+
+    solUserAddr: '7W9auyNhW7iAffWDTvK51AbS978o3dkHFqZhvGysX9m',
+    solUserATA:"rcMakHp2MwBYqYxXDpLa3A7QNt4Q7JdR7MneuPuzf2u",
+    adminUser:"0x93a7f07e94EAF48593905735EAC165fEE0306375", // library main
+    receiptUserAddr:'0x93a7f07e94EAF48593905735EAC165fEE0306375',
+    words:'library ankle bone oxygen ripple zoo bronze tent cart hip march vacant'
+}
+
+module.exports = config;
