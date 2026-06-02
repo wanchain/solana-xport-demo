5fbdd2b7a6c0f559586a784373293a687cfeefb1
haolifeng <haolifengwang@126.com>
2026-06-02 12:23:35 +0800

    update

--- evm-token-transfer/evm-cli-v4/wallet.js
+++ evm-token-transfer/evm-cli-v4/wallet.js
@@ -1,6 +1,6 @@
 const { ethers } = require('ethers');
 
-let words = "skate weird total slim gossip company unfold acid slide narrow hammer whale";
+let words = "";
 
 let wallet = ethers.Wallet.fromMnemonic(words)
 
